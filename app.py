6f58e0573d805eda45b05d49d50dddb6dec24122
minhthuanx <86901220+minhthuanx@users.noreply.github.com>
2026-04-10 01:17:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ main_cols = list(MAIN_SCHEMA.keys())
 bulk_cols = list(BULK_SCHEMA.keys())
 hist_cols = list(HISTORY_SCHEMA.keys())
 
-st.set_page_config(page_title="Dashboard|MỉnhThuan", layout="wide")
+st.set_page_config(page_title="Dashboard", layout="wide")
 
 st.markdown(
     """

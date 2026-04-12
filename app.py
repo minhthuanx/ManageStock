e6ee07cd2c2f7c8ac1b3063c548482258b9d7af4
minhthuanx <86901220+minhthuanx@users.noreply.github.com>
2026-04-13 06:16:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
     page_title="GhostlyStock",
     page_icon="👻",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 # =============================================================================
